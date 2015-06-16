84689675cdc6c687b8c89f2eec04b83b92e6485a
steveriggins <steve@steveriggins.net>
2015-06-16 00:45:48 -0700

    Return 2001

---
+++ SubmoduleA.h
@@ -0,0 +1,15 @@
+//
+//  SubmoduleA.h
+//  xcode-ci
+//
+//  Created by Steven W. Riggins on 6/16/15.
+//  Copyright © 2015 Steve Riggins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SubmoduleA : NSObject
+
+- (NSUInteger)submoduleA;
+
+@end
---
+++ SubmoduleA.m
@@ -0,0 +1,18 @@
+//
+//  SubmoduleA.m
+//  xcode-ci
+//
+//  Created by Steven W. Riggins on 6/16/15.
+//  Copyright © 2015 Steve Riggins. All rights reserved.
+//
+
+#import "SubmoduleA.h"
+
+@implementation SubmoduleA
+
+- (NSUInteger)submoduleA
+{
+    return 2001;
+}
+
+@end
